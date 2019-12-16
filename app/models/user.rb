f4ec5be4dfc4e3d15352dde41ebf5d0c9e433124
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-12-16 15:33:02 +0000

    More validation for all of the user params, but more specific tests for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  validates :username, presence: true
+  validates :username, presence: true, length: {in: 6..15}
+  validates :password, presence: true
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
