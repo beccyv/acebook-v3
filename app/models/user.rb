8af7297bbc3df135dad37dc2437f7e8ebc0dfcac
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-12-16 15:54:47 +0000

    email validationx2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ end
 class User < ApplicationRecord
   validates :username, presence: true, length: {in: 6..15}, uniqueness: true
   validates :password, presence: true, length: {in: 6..10}
-  validates :email, presence: true, length: {maximum: 255}, uniqueness: true
+  validates :email, presence: true, length: {maximum: 255}, uniqueness: true, email: true
   validates :first_name, presence: true, length: {maximum: 20}
   validates :last_name, presence: true, length: {maximum: 20}
 end
