2a5df9b90aceaa12b3bacfd08a73fa00b110b711
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-12-16 15:48:05 +0000

    Test run with validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
+# class EmailValidator < ActiveModel::EachValidator
+#   def validate_each(record, attribute, value)
+#     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+#       record.errors[attribute] << (options[:message] || "is not an email")
+#     end
+#   end
+# end
+
+
 class User < ApplicationRecord
-  validates :username, presence: true, length: {in: 6..15}
-  validates :password, presence: true
-  validates :email, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :username, presence: true, length: {in: 6..15}, uniqueness: true
+  validates :password, presence: true, length: {in: 6..10}
+  validates :email, presence: true, length: {maximum: 255}, uniqueness: true
+  validates :first_name, presence: true, length: {maximum: 20}
+  validates :last_name, presence: true, length: {maximum: 20}
 end
