42bb1fa2e2d691c08091df478995a691bc5a9ef7
lealle <leigh.leal@gmail.com>
2019-12-19 11:31:55 +0000

    passing tests

--- spec/features/links_spec.rb
+++ spec/features/links_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 
 RSpec.feature "Admin can click on link new user", type: :feature do
-  scenario "Udmin can click on link new user" do
+  scenario "Admin can click on link new user" do
+    visit "/"
+    click_link("Sign Up")
+    fill_in('First name', with: 'Peter')
+    fill_in('Last name', with: 'Boom')
+    fill_in('Email', with: 'peter1@gmail.com')
+    fill_in('Username', with: 'username')
+    fill_in('Password', with: 'password')
+    fill_in('Password confirmation', with: 'password')
+    click_button('Save User')
+
+    click_link('Log In')
+    fill_in('Username', with: 'username')
+    fill_in('Password', with: 'password')
+    click_button('Log in')
+
     visit "/users"
     click_link('New user')
     expect(page).to have_content('New User')
   end
-end 
+end
--- spec/features/newuser_spec.rb
+++ spec/features/newuser_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.feature "New User can sign up", type: :feature do
   scenario "User can signup for Acebook" do
     visit "/"
+    click_link("Sign Up")
+    fill_in('First name', with: 'Peter')
+    fill_in('Last name', with: 'Boom')
+    fill_in('Email', with: 'peter1@gmail.com')
     fill_in('Username', with: 'username')
     fill_in('Password', with: 'password')
     fill_in('Password confirmation', with: 'password')
-    fill_in('Email', with: 'peter1@gmail.com')
-    fill_in('First name', with: 'Peter')
-    fill_in('Last name', with: 'Boom')
     click_button('Save User')
 
     click_link('Log In')
@@ -19,13 +20,14 @@ RSpec.feature "New User can sign up", type: :feature do
   end
 
     scenario "User can signup for Acebook" do
-      visit "/users/new"
+      visit "/"
+      click_link "Sign Up"
+      fill_in('First name', with: 'Peter')
+      fill_in('Last name', with: 'Boom')
+      fill_in('Email', with: 'doodooface@gmail.com')
       fill_in('Username', with: 'po')
       fill_in('Password', with: 'password')
       fill_in('Password confirmation', with: 'password')
-      fill_in('Email', with: 'doodooface@gmail.com')
-      fill_in('First name', with: 'Peter')
-      fill_in('Last name', with: 'Boom')
       click_button('Save User')
       expect(page).to have_content('Username is too short')
     end
@@ -43,43 +45,44 @@ RSpec.feature "New User can sign up", type: :feature do
   end
 
   scenario "User can signup for Acebook" do
-    visit "/users/new"
+    visit "/signup"
+    fill_in('First name', with: 'Peter')
+    fill_in('Last name', with: 'Boom')
+    fill_in('Email', with: 'doodooface')
     fill_in('Username', with: 'po')
     fill_in('Password', with: 'hfkghskbfkegbkgkqegfkgwekc')
     fill_in('Password confirmation', with: 'hfkghskbfkegbkgkqegfkgwekc')
-    fill_in('Email', with: 'doodooface')
-    fill_in('First name', with: 'Peter')
-    fill_in('Last name', with: 'Boom')
     click_button('Save User')
-    expect(page).to have_content('Email is not an email')
+    expect(page).to have_content('Email is not valid. Emails must be in standard format')
   end
 
 scenario "User can signup for Acebook" do
-  visit "/users/new"
+  visit "/signup"
+  fill_in('First name', with: 'Fkghskbfkegbkgkqegfkgwekc')
+  fill_in('Last name', with: 'Boom')
+  fill_in('Email', with: 'doodooface')
   fill_in('Username', with: 'po')
   fill_in('Password', with: 'hfkghskbfkegbkgkqegfkgwekc')
   fill_in('Password confirmation', with: 'hfkghskbfkegbkgkqegfkgwekc')
-  fill_in('Email', with: 'doodooface')
-  fill_in('First name', with: 'Fkghskbfkegbkgkqegfkgwekc')
-  fill_in('Last name', with: 'Boom')
   click_button('Save User')
   expect(page).to have_content('First name is too long')
 end
 
 scenario "User can signup for Acebook" do
-  visit "/users/new"
+  visit "/signup"
+  fill_in('First name', with: 'Fkghskbfkegbkgkqegfkgwekc')
+  fill_in('Last name', with: 'Fkghskbfkegbkgkqegfkgwekchfg')
+  fill_in('Email', with: 'doodooface')
   fill_in('Username', with: 'po')
   fill_in('Password', with: 'hfkghskbfkegbkgkqegfkgwekc')
   fill_in('Password confirmation', with: 'hfkghskbfkegbkgkqegfkgwekc')
-  fill_in('Email', with: 'doodooface')
-  fill_in('First name', with: 'Fkghskbfkegbkgkqegfkgwekc')
-  fill_in('Last name', with: 'Fkghskbfkegbkgkqegfkgwekchfg')
   click_button('Save User')
   expect(page).to have_content('Last name is too long')
 end
 
 scenario "Admin can view user list" do
-  visit "/users/new"
+  visit "/"
+  click_link "Sign Up"
   fill_in('Username', with: 'username')
   fill_in('Password', with: 'password')
   fill_in('Password confirmation', with: 'password')
@@ -87,18 +90,22 @@ scenario "Admin can view user list" do
   fill_in('First name', with: 'Peter')
   fill_in('Last name', with: 'Boom')
   click_button('Save User')
+  click_link('Log In')
+  fill_in('Username', with: 'username')
+  fill_in('Password', with: 'password')
+  click_button('Log in')
   visit "/users"
   expect(page).to have_content('Boom')
 end
 
 scenario "User can signup for Acebook" do
-  visit "/users/new"
+  visit "/signup"
+  fill_in('First name', with: 'Peter')
+  fill_in('Last name', with: 'Boom')
+  fill_in('Email', with: 'peter1@gmail.com')
   fill_in('Username', with: 'username')
   fill_in('Password', with: 'password')
   fill_in('Password confirmation', with: 'password')
-  fill_in('Email', with: 'peter1@gmail.com')
-  fill_in('First name', with: 'Peter')
-  fill_in('Last name', with: 'Boom')
   click_button('Save User')
 
   click_link('Log In')
@@ -106,19 +113,18 @@ scenario "User can signup for Acebook" do
   fill_in('Password', with: 'password')
   click_button('Log in')
   visit "/"
-  #expect(page).to have_content('First Name: Peter')
   click_link('Log Out')
     expect(page).to have_content('Logged out!')
   end
 
   scenario "Admin can click on show link" do
     visit "/users/new"
+    fill_in('First name', with: 'Peter')
+    fill_in('Last name', with: 'Boom')
+    fill_in('Email', with: 'peter1@gmail.com')
     fill_in('Username', with: 'username')
     fill_in('Password', with: 'password')
     fill_in('Password confirmation', with: 'password')
-    fill_in('Email', with: 'peter1@gmail.com')
-    fill_in('First name', with: 'Peter')
-    fill_in('Last name', with: 'Boom')
     click_button('Save User')
 
     click_link('Log In')
@@ -131,9 +137,8 @@ scenario "User can signup for Acebook" do
       expect(page).to have_content('Add a post:')
   end
 
-  scenario "testing webhelper" do
-    sign_up
-    log_in
-    expect(page).to have_content('Last Name: Boom')
-end
-end
+  scenario "Redirect if not logged in" do
+  visit "/users"
+  expect(page).to have_content('Welcome to Acebook')
+    end
+  end
