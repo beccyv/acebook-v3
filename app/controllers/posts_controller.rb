41184891d4119d47d22aec86daea9facabe45736
djlonboy <djlonboy@gmail.com>
2019-12-19 11:35:21 +0000

    Implemented delete for posts. Tried to implement edit but currently not working.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,30 @@ class PostsController < ApplicationController
   redirect_to user_path(@user)
 end
 
+def edit
+  @user = User.find(params[:user_id])
+  @post = @user.posts.find(params[:id])
+end
+
+def update
+  # @post = Post.find(params[:id])
+  @user = User.find(params[:user_id])
+  @post = @user.posts.find(params[:id])
+
+  if @post.update(post_params)
+    redirect_to user_path(@user)
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @user = User.find(params[:user_id])
+  @post = @user.posts.find(params[:id])
+  @post.destroy
+  redirect_to user_path(@user)
+end
+
 private
   def post_params
     params.require(:post).permit(:post)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'welcome/index'
+  get 'posts/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :users do
